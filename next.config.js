99e7594290c6f90705a523cae5c10b1c57443a1d
Greg Potts <potts.g@ptc.edu>
2019-03-04 14:56:39 -0500

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const withCSS = require('@zeit/next-css');
+
+console.log(process.env.NODE_ENV);
+
 module.exports = withCSS({
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/wedding' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
 });
