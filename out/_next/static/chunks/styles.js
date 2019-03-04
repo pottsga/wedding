6a474b7d603448db925af72f49961b5a69cb3892
Greg Potts <potts.g@ptc.edu>
2019-03-04 14:47:15 -0500

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1551728560520");
+            injectCss(link, link.href.split("?")[0] + "?unix=1551728816131");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1551728561047");
+            injectCss(link, link.href.split("?")[0] + "?unix=1551728816653");
           });
       }
     }
